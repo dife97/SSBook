7b07d0791816e27919f6a127e36c729c125a62b3
Diego Ferreira <diferodrigues@gmail.com>
2022-10-15 20:46:55 -0300

    Remove Storyboard

---
+++ SSBook/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SSBook
+//
+//  Created by Diego Ferreira on 15/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemOrange
+    }
+}
